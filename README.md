d128af58d62cce3b21fc17344256e0b0b5df8c59
Juan Ruiz Echazu <hey@juan-i.com.ar>
2013-07-08 23:37:57 -0300

    customize shadows! closes #2

--- README.md
+++ README.md
@@ -3,14 +3,16 @@ iluminate
 
 Add those trendy flat shadows on stuff. Needs https://github.com/infusion/jQuery-xcolor to work.
 
-Set up size(int for pixels) and dropoff (0-1).
-
-For example: 
-$(".enlighten").iluminate({
-  size         : 100,
-  dropoffStart : 0.6
-});
-
-Child spans will also get a shadow.
+```$(".enlighten").iluminate({
+  direction    : "bottomRight",		// "top", "topLeft", "topRight", "left", "right","bottom", "bottomLeft". defaults to "bottomRight"
+  size         : 100, 				// box-shadow distance, in pixels
+  textSize     : 100,				// text-shadow distance, in pixels
+  fade         : 0.6,				// point at which the box-shadow starts to fade-out. 0-1
+  fadeText     : 0.6,				// point at which the text-shadow starts to fade-out. 0-1
+  alpha        : 0.05,				// box-shadow opacity. 0-1
+  textAlpha    : 0.05, 				// text-shadow opacity. 0-1
+  includeText  : true,				// if false, only text wrapped by spans will have a shadow
+  textOnly     : false				// use to only get shadows on text
+});```
 
 Demo: http://sandbox.juan-i.com/flat/
--- jquery.iluminate.js
+++ jquery.iluminate.js
@@ -1,34 +1,66 @@
 (function($) {
  
-    $.fn.iluminate = function( options ) {
+    $.fn.enlighten = function( options ) {
+
+        var elSize = $(this).height();
 
         var settings = $.extend({
             size         : 100,
-            dropoffStart : 0.6
+            direction    : "bottomRight",
+            textSize     : elSize/2,
+            fade         : null,
+            fadeText     : null,
+            includeText  : true,
+            textOnly     : false,
+            alpha        : 0.05,
+            textAlpha    : 0.05
         }, options);
 
         return this.each( function() {
-            var elSize = $(this).height();
 
             // parent bg color will determine the shadow color
             var bg = $(this).parent().css("background-color");
-            var darkerBG = $.xcolor.opacity(bg, 'black', 0.05);
+            var darkerBG = $.xcolor.opacity(bg, 'black', settings.alpha);
 
             // generate shadows and offset
             var shadows = [];
             var shadowSize = settings.size;
-            var dropoffStart = Math.ceil(shadowSize*settings.dropoffStart);
+            var fade = Math.ceil(shadowSize*settings.fade);
 
             for (var i = 0; i < shadowSize; i++) {
-
-                if (i <= dropoffStart) {
-                    var tweenedBG = $.xcolor.gradientlevel(darkerBG, bg, dropoffStart-i, dropoffStart);
-                    shadow = (shadowSize - i) + "px " + (shadowSize - i) + "px " + tweenedBG;
+                if (settings.direction == "top") {
+                    var shadowX = 0;
+                    var shadowY = (shadowSize - i)*-1;
+                } else if (settings.direction == "topRight") {
+                    var shadowX = shadowSize - i;
+                    var shadowY = (shadowSize - i)*-1;
+                } else if (settings.direction == "right") {
+                    var shadowX = shadowSize - i;
+                    var shadowY = 0;
+                } else if (settings.direction == "bottom") {
+                    var shadowX = 0;
+                    var shadowY = shadowSize - i;
+                } else if (settings.direction == "bottomLeft") {
+                    var shadowX = (shadowSize - i)*-1;
+                    var shadowY = shadowSize - i;
+                } else if (settings.direction == "left") {
+                    var shadowX = (shadowSize - i)*-1;
+                    var shadowY = 0;
+                } else if (settings.direction == "topLeft") {
+                    var shadowX = (shadowSize - i)*-1;
+                    var shadowY = (shadowSize - i)*-1;
+                } else {
+                    var shadowX = shadowSize - i;
+                    var shadowY = shadowSize - i;
+                }
+                var shadow = shadowX + "px " + shadowY + "px ";
+                if ( settings.fade && i <= fade) {
+                    var tweenedBG = $.xcolor.gradientlevel(darkerBG, bg, fade-i, fade);
+                    shadow += tweenedBG;
                 }
                 else {
-                    shadow = (shadowSize - i) + "px " + (shadowSize - i) + "px " + darkerBG;
+                    shadow += darkerBG;
                 }
-
                 shadows.push(shadow);
             }
 
@@ -36,19 +68,51 @@
             var shadowString = shadows.reverse().join();
 
             // we'll need this element's background to set the text shadow color
-            var txc = $(this).css("background-color");
-            var darkerTX = $.xcolor.opacity(txc, 'black', 0.05);
-            var convertedTX = darkerTX.getRGB();
-            var RGBarrayTX = [];
-            $.each(convertedTX, function(key, value){
-                RGBarrayTX.push(value);
-            });
-            var RGBvaluesTX = RGBarrayTX[0] + ", " + RGBarrayTX[1] + ", " + RGBarrayTX[2];
+            if ( $(this).css("background-color") == "rgba(0, 0, 0, 0)" ) {
+                var txc = $(this).parent().css("background-color");
+            } else {
+                var txc = $(this).css("background-color");   
+            }
+            var darkerTX = $.xcolor.opacity(txc, 'black', settings.textAlpha);
 
             // generate shadows and offset
             var textShadows = [];
-            for (var i = 0; i < elSize/2; i++) {
-                textShadow =  " " + (elSize/2 - i) + "px " + (elSize/2 - i) + "px rgb(" + RGBvaluesTX + ")";
+            var fadeText = Math.ceil(settings.textSize*settings.fadeText);
+
+            for (var i = 0; i < settings.textSize; i++) {
+                if (settings.direction == "top") {
+                    var textX = 0;
+                    var textY = (settings.textSize - i)*-1;
+                } else if (settings.direction == "topRight") {
+                    var textX = settings.textSize - i;
+                    var textY = (settings.textSize - i)*-1;
+                } else if (settings.direction == "right") {
+                    var textX = settings.textSize - i;
+                    var textY = 0;
+                } else if (settings.direction == "bottom") {
+                    var textX = 0;
+                    var textY = settings.textSize - i;
+                } else if (settings.direction == "bottomLeft") {
+                    var textX = (settings.textSize - i)*-1;
+                    var textY = settings.textSize - i;
+                } else if (settings.direction == "left") {
+                    var textX = (settings.textSize - i)*-1;
+                    var textY = 0;
+                } else if (settings.direction == "topLeft") {
+                    var textX = (settings.textSize - i)*-1;
+                    var textY = (settings.textSize - i)*-1;
+                } else {
+                    var textX = settings.textSize - i;
+                    var textY = settings.textSize - i;
+                }
+                var textShadow = textX + "px " + textY + "px ";
+                if ( settings.fadeText && i <= fadeText) {
+                    var tweenedTX = $.xcolor.gradientlevel(darkerTX, txc, fadeText-i, fadeText);
+                    textShadow += tweenedTX;
+                }
+                else {
+                    textShadow += darkerTX;
+                }
                 textShadows.push(textShadow);
             }
 
@@ -56,8 +120,24 @@
             var textShadowString = textShadows.reverse().join();
 
             // apply shadows
-            $(this).css("box-shadow", shadowString);
-            $(this).find("span").css("text-shadow", textShadowString);
+            if ( settings.includeText ) {
+                $(this).css({
+                    "box-shadow": shadowString,
+                    "text-shadow": textShadowString,
+                    "overflow": "hidden"
+                });
+            } else {
+                $(this).css("box-shadow", shadowString);
+                $(this).find("span").css("text-shadow", textShadowString);
+            }
+
+            if ( settings.textOnly ) {
+                $(this).css({
+                    "overflow": "initial",
+                    "text-shadow": textShadowString,
+                    "box-shadow": "none"
+                });
+            }
         });
  
     };
